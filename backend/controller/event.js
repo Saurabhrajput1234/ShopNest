12d3f06e4e7d1dbd82985f251c2f04f6bf723800
saurabh <saurabh2113076@akgec.ac.in>
2025-03-27 23:49:52 +0530

    Fix Vercel deployment with correct project name

--- backend/controller/event.js
+++ backend/controller/event.js
@@ -44,17 +44,17 @@ router.post(
   })
 );
 // Get all events
-// router.get("/get-all-events", async (req, res, next) => {
-//   try {
-//     const events = await Event.find();
-//     res.status(201).json({
-//       success: true,
-//       events,
-//     });
-//   } catch (error) {
-//     return  next(new ErrorHandler(error, 400));
-//   }
-// });
+router.get("/get-all-events", async (req, res, next) => {
+  try {
+    const events = await Event.find();
+    res.status(201).json({
+      success: true,
+      events,
+    });
+  } catch (error) {
+    return  next(new ErrorHandler(error, 400));
+  }
+});
 
 // Get all events of a shop
 router.get(
